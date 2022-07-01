5b81ccee88238a7c51447e0566845bba39dab816
Slo <c1boiteamail@gmail.com>
2022-07-01 17:56:24 +0200

    config heroku

--- migrations/Version20220701155232.php
+++ migrations/Version20220701155232.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220628124252 extends AbstractMigration
+final class Version20220701155232 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220628124252 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE vehicule CHANGE photo photo VARCHAR(255) NOT NULL');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE vehicule CHANGE photo photo VARCHAR(255) DEFAULT NULL');
+
     }
 }
